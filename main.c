c157ccdabdda1eacf455c25f3d491a43e6c97090
Pablix44737 <pabloluna44737@gmail.com>
2024-05-20 18:13:50 -0300

    Actualizar main.c

--- main.c
+++ main.c
@@ -4,6 +4,20 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+struct tecnico{
+     int id;
+     long int DNI;
+     char Nombre[30];
+     struct tecnico *sgte;
+};
+
+struct cliente{
+     int id;
+     long int DNI;
+     char Nombre[30];
+     struct cliente *sgte;
+};
+
 struct tarea{
      int id_op,id_tarea,orden;
      float tiempo;
@@ -12,17 +26,26 @@ struct tarea{
 };
 
 struct materiales{
+     int id;
      char descripcion[30];
      float costo_uni,cantidad;
      struct materiales *sgte;
 };
 
 struct opcion{
+     int id;
      char Nombre[30];
      float costo;
      struct tarea *ini;
      struct materiales *ini;
      struct opcion *sgte;
+};
+
+struct trabajos{
+     int id_trabajo,id_opcion,may4;
+     char direccion[30];
+     int id_tecnico, id_cliente;
+     struct trabajos *sgte;
 };
 
 int main(int argc, char *argv[]){
