e69f93e6d53967d804f943eb3df617f1f27c9fd6
Pablix44737 <pabloluna44737@gmail.com>
2024-05-20 17:51:45 -0300

    Actualizar main.c

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct tarea{
+     int id_op,id_tarea,orden;
+     float tiempo;
+     char descripcion[30];
+     struct tareas *sgte;
+};
+
+struct opcion{
+     char Nombre[30];
+     float costo;
+     struct opcion *sgte;
+};
 
 int main(int argc, char *argv[]) {
 	return 0; guu
