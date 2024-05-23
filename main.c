b53719b319473b5f7e7af8993fc107f7d5f0aabc
Amonette <106854948+JeanetteP27@users.noreply.github.com>
2024-05-23 03:44:33 -0300

    Update main.c
    nose como se usa esto, espero no borrar nada.

--- main.c
+++ main.c
@@ -58,7 +58,79 @@ struct trabajos{
      struct trabajos *sgte;
 };
 
-int main(int argc, char *argv[]){
+int menu (int opc); //menusito
+void listado_opc (); //para listar opciones
+void alta_opc (); //para dar de alta opciones
+void alta_trab (); //para dar de alta trabajos
+void listado_trabytar (); //para lisar trabajos pendientes
+void listado_finitrab (); //para listar trabajos finalizados
+void mopcsvends (); //para listar opciones mas vendidas
 
-	return 0;
+int main(int argc, char *argv[]){
+	int opc=-1;
+	while(opc!=0){
+		opc=menu(opc);
+		switch(opc){
+			case 1:
+				listado_opc();
+				opc=-1; //NO BORRAR hasta que carguemos la funcion.
+				break;
+			case 2:
+				alta_opc();
+				opc=-1; //NO BORRAR hasta que carguemos la funcion.
+				break;
+			case 3:
+				alta_trab();
+				opc=-1; //NO BORRAR hasta que carguemos la funcion.
+				break;
+			case 4:
+				listado_trabytar();
+				opc=-1; //NO BORRAR hasta que carguemos la funcion.
+				break;
+			case 5:
+				listado_finitrab();
+				opc=-1; //NO BORRAR hasta que carguemos la funcion.
+				break;
+			case 6:
+				mopcsvends();
+				opc=-1; //NO BORRAR hasta que carguemos la funcion.
+				break;
+		}
+	}
+	return (0);
+}
+
+int menu (int o){
+	int contgency=0;
+	while( (o!=0) && (o!=1) && (o!=2) && (o!=3) && (o!=4) && (o!=5) && (o!=6) ){
+		if(contgency>=1){
+			printf( "El valor que ingreso no es valido, vuelva a ingresar una opcion. \n" );
+			scanf( "%d", &o );
+			fflush(stdin);
+		}else{
+			printf( "-------------------Bienvenido al menu :D-------------------\n---Ingrese 1 para listar opciones.\n---Ingrese 2 para dar de alta un opcion.\n---Ingrese 3 para dar de alta un trabajo.\n---Ingrese 4 para listar trabajos y tareas pendientes.\n---Ingrese 5 para listar los trabajos finalizados.\n---Ingrese 6 para listar las opciones mas vendidas.\n" );
+			scanf( "%d", &o );
+			fflush(stdin);
+			contgency++;
+		}
+	}
+	return (o);
+}
+
+void listado_opc (){
+}
+
+void alta_opc (){
+}
+
+void alta_trab (){
+}
+
+void listado_trabytar (){
+}
+
+void listado_finitrab (){
+}
+
+void mopcsvends (){
 }
