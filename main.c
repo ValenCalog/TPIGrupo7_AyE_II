db1919b8262ac61787072dcc8b3f055b09d94d5a
Amonette <106854948+JeanetteP27@users.noreply.github.com>
2024-05-24 03:21:01 -0300

    Update main.c
    Cambios: modifique los nombres de las funciones, ordene alfabéticamente estructuras, prototipos y módulos (a los prototipos y módulos les cambie el nombre porque me di cuenta que nos íbamos a confundir todo después). Retoque algunas partes del código para que se lea mejor.
    
    Añadidos: Agregué OperaciónTiempo y OperaciónCosto que antes eran tiempo_op y costo_op de pablo, solo para que no saltara error al compilar y quedara capado.

--- main.c
+++ main.c
@@ -2,60 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 struct fecha{
-     int dia,mes,anio; //Inicializamos en 0?
+     int dia,mes,anio;
 };
 
-struct tecnico{ //cola
-     int id;
-     long int DNI;
-     char Nombre[30];
-     struct tecnico *sgte;
-};
-
-struct cliente{ //cola
-     int id;
-     long int DNI;
-     char Nombre[30];
-     struct cliente *sgte;
-};
-
-struct tarea{ //lista doblemente enlazada
-     int id_op,id_tarea,orden;
-     float tiempo;
-     char descripcion[30];
-     struct tarea *sgte, *ant;
-};
-
-struct materiales{ //arbol de busqueda binaria
+struct materiales{ //Arbol de Busqueda Binaria.
      int id;
      char descripcion[30],unimed[10];
      float costo_uni,cantidad;
      struct materiales *izq, *derch;
 };
 
-struct materialesop{
-	int idmat, cantidad,id_opcion;
-	struct materialesop *sgte;
-};
-
-struct opcion{ //lista enlazada simple
-     int id;
-     char Nombre[30];
-     float costo, tiempo;
-     struct opcion *sgte;
-};
-
-struct pendientes{ //pila
-	int id_trabajo,orden;
-	float tiempo;
-    char descripcion[30];
-    struct pendientes *sgte;
-};
-
-struct trabajos{ 
+struct trabajos{ //Cola.
      int id_trabajo,id_opcion,cuatromtrs;
      char direccion[30];
      int id_tecnico, id_cliente;
@@ -63,50 +21,93 @@ struct trabajos{
      struct trabajos *sgte;
 };
 
-int menu (int opc); //menusito
-void listado_opc (); //para listar opciones
-void alta_opc (); //para dar de alta opciones
-void alta_trab (); //para dar de alta trabajos
-void listado_trabytar (); //para lisar trabajos pendientes
-void listado_finitrab (); //para listar trabajos finalizados
-void mopcsvends (); //para listar opciones mas vendidas
-void altamateriales(struct materiales *r);
-struct materiales* insertarnuevomat(struct materiales *r, struct materiales *nodo);
+struct tarea{ //Lista Doblemente Enlazada.
+     int id_op,id_tarea,orden;
+     float tiempo;
+     char descripcion[30];
+     struct tarea *sgte, *ant;
+};
+
+struct cliente{ //Lista Enlazada Simple.
+     int id;
+     long int DNI;
+     char Nombre[30];
+     struct cliente *sgte;
+};
+
+struct materialesop{ //Lista Enlazada Simple.
+	int idmat, cantidad,id_opcion;
+	struct materialesop *sgte;
+};
+
+struct opcion{ //Lista Enlazada Simple.
+     int id;
+     char Nombre[30];
+     float costo, tiempo;
+     struct opcion *sgte;
+};
+
+struct tecnico{ //Lista Enlazada Simple.
+     int id;
+     long int DNI;
+     char Nombre[30];
+     struct tecnico *sgte;
+};
+
+struct pendientes{ //Pila
+	int id_trabajo,orden;
+	float tiempo;
+    char descripcion[30];
+    struct pendientes *sgte;
+};
+
+float OperacionTiempo (int id);
+float OperacionCosto (float tiempo);
+int Menu (int opc);
+struct materiales* InsertarNuevoMaterial (struct materiales *r, struct materiales *nodo);
+void AltaDeMateriales (struct materiales *r);
+void AltaDeOpciones ();
+void AltaDeTrabajos ();
+void InsertarOpcion (struct opcion *nueva_op);
+void ListadoDeOpciones ();
+void ListadoDePendientes ();
+void ListadoDeFinalizados ();
+void OpcionesMasVendidas ();
 
 int main(int argc, char *argv[]){
-	struct materiales *r, *izq, *der;
+	struct materiales *r;
 	int opc=-1;
 	
 	while(opc!=0){
 		//system("CLS");
-		opc=menu(opc);
+		opc = Menu(opc);
 		switch(opc){
 			case 1:
-				listado_opc();
+				ListadoDeOpciones();
 				opc=-1; //NO BORRAR hasta que carguemos la funcion.
 				break;
 			case 2:
-				alta_opc();
+				AltaDeOpciones();
 				opc=-1; //NO BORRAR hasta que carguemos la funcion.
 				break;
 			case 3:
-				alta_trab();
+				AltaDeTrabajos();
 				opc=-1; //NO BORRAR hasta que carguemos la funcion.
 				break;
 			case 4:
-				listado_trabytar();
+				ListadoDePendientes();
 				opc=-1; //NO BORRAR hasta que carguemos la funcion.
 				break;
 			case 5:
-				listado_finitrab();
+				ListadoDeFinalizados();
 				opc=-1; //NO BORRAR hasta que carguemos la funcion.
 				break;
 			case 6:
-				mopcsvends();
+				OpcionesMasVendidas();
 				opc=-1; //NO BORRAR hasta que carguemos la funcion.
 				break;
 			case 7:
-				altamateriales (r);
+				AltaDeMateriales(r);
 				opc=-1; //NO BORRAR hasta que carguemos la funcion.
 				break;
 		}
@@ -114,7 +115,13 @@ int main(int argc, char *argv[]){
 	return (0);
 }
 
-int menu (int o){
+float OperacionTiempo (int id){
+}
+
+float OperacionCosto (float tiempo){
+}
+
+int Menu (int o){
 	int contgency=0;
 	while( (o!=0) && (o!=1) && (o!=2) && (o!=3) && (o!=4) && (o!=5) && (o!=6) ){
 		if(contgency>=1){
@@ -131,80 +138,81 @@ int menu (int o){
 	return (o);
 }
 
-void listado_opc (){
-	struct opcion{
-     int id;
-     char Nombre[30];
-     float costo;
-     struct tarea *ini;
-     struct materiales *iniMat;
-     struct opcion *sgte;
-	};
-}
-
-void alta_opc (){
-	struct opcion nueva_op;
-	nueva_op = (struct opcion *) malloc(sizeof(struct opcion));
-	if (nueva_op!=NULL){
-		printf("Digite el ID de la opcion: ");
-		scanf("%i",&nueva_op->id);
-		printf("Digite el nombre de la nueva opcion: ");
-		gets(nueva_op->Nombre);
-		nueva_op->tiempo= tiempo_op(nueva_op->id);
-		nueva_op->costo= costo_op(nueva_op->tiempo);
-		nueva_op->sgte=NULL;
-		insertarop(nueva_op);
+struct materiales* InsertarNuevoMaterial (struct materiales *raiz, struct materiales *nodo){
+	if( raiz == NULL ){
+		raiz = nodo;
 	}else{
-		printf("No hay espacio en memoria");
-	}
-}
-
-void alta_trab (){
-}
-
-void listado_trabytar (){
-}
-
-void listado_finitrab (){
-}
-
-void mopcsvends (){
-}
-
-void altamateriales(struct materiales *raiz){
-    struct materiales *nuevo_mat;
-
-    nuevo_mat= (struct materiales *) malloc(sizeof(struct materiales));
-    if (nuevo_mat == NULL){
-       printf("No hay espacio en la memoria /n");
-    } else {
-    	printf("Ingrese el ID de material: ");
-     scanf("%i",&nuevo_mat->id)
-     //nuevo_mat->id = buscarmayorid() + 1; necesitamos saber el id del ultimo material ingresado
-     printf("Ingrese la descripcion del material: ");
-     gets(nuevo_mat->descripcion);
-     printf("Ingrese cual sera la unidad de medida: ");
-     gets(nuevo_mat->unimed);
-     printf("Ingrese el precio unitario del nuevo material: ");
-     scanf("%f",&nuevo_mat->costo_uni);
-     raiz = insertarnuevomat(raiz, nuevo_mat);//insertamos el nodo con el nuevo material al arbol
-    }
-}
-
-struct materiales* insertarnuevomat(struct materiales *raiz, struct materiales *nodo){
-     
-    if (raiz != NULL){
-     	if (raiz == nodo){
-     		printf("Ya está en la lista. \n");
-		} else {
-			if (nodo<raiz){
-				raiz->izq = insertarnuevomat(raiz->izq,nodo);
-			} else {
-				raiz->derch = insertarnuevomat(raiz->derch,nodo);
+		if( raiz == nodo ){
+			printf( "Ya esta en la lista. \n" );
+		}else{
+			if( nodo < raiz ){
+				raiz->izq = InsertarNuevoMaterial (raiz->izq, nodo);
+			}else{
+				raiz->derch = InsertarNuevoMaterial (raiz->derch, nodo);
 			}
 		}
-	} else {
-		raiz=nodo;
 	}
 	return (raiz);
+}
+
+void AltaDeMateriales (struct materiales *raiz){
+    struct materiales *nuevo_mat;
+
+    nuevo_mat = (struct materiales *) malloc (sizeof (struct materiales) );
+    if( nuevo_mat == NULL ){
+       printf( ":( No hay espacio en la memoria \n" );
+    }else{
+    	printf( "---Ingrese el ID de material: \n" );
+		scanf( "%i", &nuevo_mat->id );
+		
+		printf( "---Ingrese la descripcion del material: \n" );
+		gets( nuevo_mat->descripcion );
+		
+		printf( "---Ingrese cual sera la unidad de medida: \n" );
+		gets( nuevo_mat->unimed );
+		
+		printf( "---Ingrese el precio unitario del nuevo material: \n" );
+		scanf( "%f", &nuevo_mat->costo_uni );
+		
+		raiz = InsertarNuevoMaterial (raiz, nuevo_mat);
+		//Insertamos el nodo, con el nuevo material, al arbol.
+	}
+}
+
+void AltaDeOpciones (){
+	struct opcion *nueva_op;
+	nueva_op = (struct opcion *) malloc (sizeof (struct opcion) );
+	if(nueva_op == NULL){
+		printf( ":( No hay espacio en memoria \n" );
+		
+	}else{
+		printf( "---Digite el ID de la opcion: \n" );
+		scanf( "%i", &nueva_op->id );
+		
+		printf( "---Digite el nombre de la nueva opcion: \n" );
+		gets( nueva_op->Nombre );
+		
+		nueva_op->tiempo = OperacionTiempo ( nueva_op->id );
+		nueva_op->costo = OperacionCosto ( nueva_op->tiempo );
+		nueva_op->sgte = NULL;
+		InsertarOpcion (nueva_op);
+	}
+}
+
+void AltaDeTrabajos (){
+}
+
+void InsertarOpcion (struct opcion *nueva_op){
+}
+
+void ListadoDeOpciones (){
+}
+
+void ListadoDePendientes (){
+}
+
+void ListadoDeFinalizados (){
+}
+
+void OpcionesMasVendidas (){
 }
